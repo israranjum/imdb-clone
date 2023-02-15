512ce9cc27cd6b3d6240ec9d4abab736923e51a6
israranjum <israranjum851@gmail.com>
2023-02-15 23:29:11 +0500

    API Card Design and card component set

--- src/app/Results.tsx
+++ src/app/Results.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import React from "react";
+import Card from "./Components/Card";
 
 interface ResultIMDB {
   id: number;
@@ -9,9 +10,9 @@ interface ResultIMDB {
 const Results = (props: any) => {
   // console.log(props.result.id);
   return (
-    <div>
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl: grid-cols-5 max-w-6xl mx-auto py-4">
       {props.results.map((result: ResultIMDB) => {
-        return <div key={result.id}>{result.original_title}</div>;
+        return <Card key={result.id} result={result} />;
       })}
     </div>
   );
