07a458f4118797019f58fbbe38dadbf83256d659
israranjum <israranjum851@gmail.com>
2023-02-20 23:42:17 +0500

    single page movie

--- src/app/Results.tsx
+++ src/app/Results.tsx
@@ -5,12 +5,19 @@ import Card from "./Components/Card";
 interface ResultIMDB {
   id: number;
   original_title: string;
+  release_date: Date;
+  backdrop_path: string;
+  poster_path: string;
+  overview: string;
+  first_air_date: Date;
+  vote_count: number;
+  title: string;
 }
 
 const Results = (props: any) => {
   // console.log(props.result.id);
   return (
-    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl: grid-cols-5 max-w-6xl mx-auto py-4">
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl: grid-cols-5 max-w-6xl mx-auto py-4 ">
       {props.results.map((result: ResultIMDB) => {
         return <Card key={result.id} result={result} />;
       })}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default async function Home({ searchParams }: { searchParams: any }) {
   const data = await res.json();
   const results = data.results;
 
-  console.log(results);
+  // console.log(results);
   return (
     <div>
       <Results results={results} />
